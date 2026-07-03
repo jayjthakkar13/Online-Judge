54a959123f21de2790ff5615ccbc7eef51b6ad1e
Jay <jayjthakkar13@gmail.com>
2026-07-03 18:14:29 +0530

    feat(auth): add auth guards and interceptors in frontend and authentication on backend for all routes

--- server/src/routes/problems.routes.ts
+++ server/src/routes/problems.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import ProblemsController from "../controllers/problems.controller";
+import authenticate from "../middleware/authenticate";
 
 const router = Router();
 
-router.get('/problemset', ProblemsController.getProblemSet);
+router.get('/problemset', authenticate, ProblemsController.getProblemSet);
 
-router.get('/problem/:problemName', ProblemsController.getProblem);
+router.get('/problem/:problemName', authenticate, ProblemsController.getProblem);
 
 export default router;
--- server/src/routes/submission.routes.ts
+++ server/src/routes/submission.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import SubmissionController from "../controllers/submissions.controller";
+import authenticate from "../middleware/authenticate";
 
 const router = Router();
 
-router.post('/submit', SubmissionController.submit);
+router.post('/submit', authenticate, SubmissionController.submit);
 
-router.get('/submission/:submissionId', SubmissionController.getSubmission);
+router.get('/submission/:submissionId', authenticate, SubmissionController.getSubmission);
 
-router.get('/submissions/:userId', SubmissionController.getAllSubmissions);
+router.get('/submissions/:userId', authenticate, SubmissionController.getAllSubmissions);
 
 export default router;
