e42ca87d5f03fae4612fb092b2538d46afe07db0
Jay <jayjthakkar13@gmail.com>
2026-08-23 17:07:58 +0530

    fix: replace backend IP with HTTPS domain

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,7 +13,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT ?? 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/auth', authRouter);
